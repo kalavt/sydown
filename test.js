fba8861db38deb165af8d8c3cf2c588aa56966ed
kalavt <443311+kalavt@users.noreply.github.com>
2021-04-12 15:23:11 +0800

    add batch transfer

---
+++ test.js
@@ -0,0 +1,17 @@
+var app = require('./app')
+console.log(app)
+app.handle_baidu({message:{text:`怦然再心动。
+链接：https://pan.baidu.com/s/1H5khORfhX1HTXrxwI9bsyQ 
+提取码：tuzi 
+
+创造营。
+链接：https://pan.baidu.com/s/1Dbl9dJXUhmReiCKfWK9BQQ 
+提取码：tuzi 
+
+老郭有新番。
+链接：https://pan.baidu.com/s/10MGUIBgVNGUvtln73n7Nzg 
+提取码：tuzi 
+
+乘风破浪的姐姐。
+链接：https://pan.baidu.com/s/1t89q4fSRR5Bqe9ev4FUOFg 
+提取码：tuzi`}})
